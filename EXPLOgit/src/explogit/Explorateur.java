b7259d43b0e0982653fa151c188f60a7b121ee55
Melchior8 <alike08@yahoo.com>
2016-02-03 16:14:24 +0100

    début 2eme fenêtre

---
+++ EXPLOgit/src/explogit/Explorateur.java
@@ -0,0 +1,28 @@
+ /* To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package explogit;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author MelchiOr'
+ */
+public class Explorateur extends JFrame{
+    public Explorateur(){
+        /*Definir taille fenetre*/
+        setSize(500, 500);
+        
+        //Nous demandons maintenant à notre objet de se positionner au centre
+        setLocationRelativeTo(null);
+        
+        //Nous demandons maintenant à notre objet de se positionner au centre
+        setLocationRelativeTo(null);
+                
+        //Termine le processus lorsqu'on clique sur la croix rouge
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
